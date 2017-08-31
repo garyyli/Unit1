87c3614f9c495d7ed744ed2f43b568cc5d870cc2
garyyli <garyli@hanovernorwichschools.org>
2017-08-31 14:50:57 -0400

    Updated from Brython Server: 8/31/2017 2:50:58 PM

--- nameAge.py
+++ nameAge.py
@@ -1,1 +1,10 @@
+#Gary Li
+#8/31/17
+#nameAge.py
 
+name = input('Enter your first and last name: ')
+age = int(input('Enter your age: '))
+firstname, lastname = name.split()
+print('Your first name has', len(firstname), 'letters')
+print('Your last name has', len(lastname), 'letters')
+print('Next year you will be', age+1, 'years old')
