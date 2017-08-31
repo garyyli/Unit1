1de5e9a0d73006f75c4c114e3ebb92f6e848db34
garyyli <garyli@hanovernorwichschools.org>
2017-08-31 14:28:34 -0400

    Updated from Brython Server: 8/31/2017 2:28:34 PM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Gary Li
+#8/31/17
+#face.py
 
+print('      XXXXX   ')
+print('     X     X   ')
+print('   ((  O  O  )) ')
+print('     |   V   |')
+print('     |  ===  |')
+print('      -------')
