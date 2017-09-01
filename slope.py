72a51724641a8bb6fa8d9bf8f971777c6d695284
garyyli <garyli@hanovernorwichschools.org>
2017-09-01 14:49:59 -0400

    Updated from Brython Server: 9/1/2017 2:49:59 PM

--- slope.py
+++ slope.py
@@ -1,1 +1,12 @@
+#Gary Li
+#9/1/17
+#slope.py
 
+x1 = float(input('Enter value for x1: '))
+x2 = float(input('Enter value for x2: '))
+y1 = float(input('Enter value for y1: '))
+y2 = float(input('Enter value for y2: '))
+slope = float(((y2-y1)/(x2-x1)))
+print('The slope of the line is', slope)
+yIntercept = float(((slope*(-1)*(x1))+y1))
+print('The equation of the line is Y =' , slope, '+', yIntercept)
