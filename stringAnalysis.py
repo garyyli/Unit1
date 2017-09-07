3dead6b73fda2cc9271f78e9f568b8e39cc2f500
garyyli <garyli@hanovernorwichschools.org>
2017-09-07 14:26:37 -0400

    Updated from Brython Server: 9/7/2017 2:26:37 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -6,3 +6,6 @@ sentence = input('Enter a sentence: ')
 
 character = input('Enter a character to search for: ')
 print('Your sentence has', sentence.count(character), 'of the character', character)
+
+word = input('Enter a word to search for: ')
+print('Your sentence has', sentence.count(word), 'of the word', word)
