380db99cb17f37462d309e9fac3b70bba8f0fe23
garyyli <garyli@hanovernorwichschools.org>
2017-09-07 14:54:30 -0400

    Updated from Brython Server: 9/7/2017 2:54:30 PM

--- change.py
+++ change.py
@@ -1,1 +1,19 @@
+#Gary Li
+#9/7/17
+#change.py
 
+cents = int(input('Input a number of cents: '))
+quarters = cents//25
+print('Quarters:', quarters)
+
+coinsAfterQuarters = cents%25
+dimes = coinsAfterQuarters//10
+print('Dimes:', dimes)
+
+coinsAfterDimes = coinsAfterQuarters%10
+nickels = coinsAfterDimes//5
+print('Nickels:', nickels)
+
+coinsAfterNickels = coinsAfterDimes%5
+pennies = coinsAfterNickels//1
+print('Pennies:', pennies)
