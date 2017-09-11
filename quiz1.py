c86820254184da2d99132adaf2e85a3efeeb61b0
garyyli <garyli@hanovernorwichschools.org>
2017-09-11 14:20:48 -0400

    Updated from Brython Server: 9/11/2017 2:20:50 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,10 @@
+#Gary Li
+#9/11/17
+#quiz1.py
 
+print('Gary Li')
+number1 = float(input('Enter a number: '))
+number2 = float(input('Enter another number: '))
+print('The sum is', number1+number2)
+print('The product is', number1*number2)
+print('Your lucky number is', (number1*7)+(number2*7))
